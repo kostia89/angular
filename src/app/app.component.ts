4916e70c5ae095668e77fcf051836d7352e447b2
kostia89 <0665761301k@gmail.com>
2021-06-13 17:42:11 +0300

    init project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,27 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Bay list';
- 
-  list = [{
-    name: 'хліб',
-    value: 1,
-    done: true
-  },
-  {
-    name: 'сало',
-    value: 0.5,
-    done: true
-  },{
-    name: 'цибуля',
-    value: 0.5,
-    done: false
-  },{
-    name: 'картопля',
-    value: 3,
-    done: false
-  }]
-  checkList(){
-    alert('ok')
-  }
+  
 }
