186d3955092deb41b3c53d0dd231d683cd8d2b91
kostia89 <0665761301k@gmail.com>
2021-06-20 22:41:17 +0300

    home-6-hopeAll

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,16 @@ import { FormBuilder, FormGroup } from '@angular/forms';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
- 
+  onLogin: boolean = true;
+  onRegistr: boolean;
   onLoginForm(){
-    
+   this.onLogin = true,
+   this.onRegistr = false
+  }
+  onRegisterForm(){
+    this.onRegistr = true,
+    this.onLogin = false
   }
 }
+
+
