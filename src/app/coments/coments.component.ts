a939904a865b2e1e03206188d28fe22b904d7a51
kostia89 <0665761301k@gmail.com>
2021-06-27 13:06:57 +0300

    home-8

--- src/app/coments/coments.component.ts
+++ src/app/coments/coments.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Coment } from '../coments.model';
 
+import { PostServService } from '../posts/post-serv.service';
+
+
 import { ComentsServiceService } from './coments-service.service';
 
 @Component({
@@ -13,16 +16,18 @@ export class ComentsComponent implements OnInit {
   coments: Coment [];
   
   constructor(
-    private Coment: ComentsServiceService
+    private Coment: ComentsServiceService,
+    private Post : PostServService
   ) { }
 
   ngOnInit(): void {
-    this.Coment.getComent().subscribe((coments: Coment [])=> {
-      console.log(coments);
-      this.coment = coments;
-      this.coments = coments;
+    // this.Coment.getComent().subscribe((coments: Coment [])=> {
+    //   // console.log(coments);
+    //   this.coment = coments;
+    //   this.coments = coments;
       
-    })
+    // })
+    this.Coment.getComent()
   }
 
 }
--- src/app/posts/posts/posts.component.ts
+++ src/app/posts/posts/posts.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { observable } from 'rxjs';
+import { ComentsServiceService } from 'src/app/coments/coments-service.service';
 import { Post } from 'src/app/post.model';
 import { PostServService } from '../post-serv.service';
-
+import { mergeMap } from 'rxjs/operators'
+import { Coment } from 'src/app/coments.model';
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
@@ -11,17 +12,36 @@ import { PostServService } from '../post-serv.service';
 export class PostsComponent implements OnInit {
   post: any;
   posts: Post [];
- 
+  coments: Coment[];
   constructor(
-    private Post : PostServService
-  ) { }
+    private Post : PostServService, 
+    private coment: ComentsServiceService
+    ) { }
 
   ngOnInit(): void {
     this.Post.getPosts().subscribe((posts: Post [])=> {
       console.log(posts);
       this.post = posts;
       this.posts = posts;
+      
     })
+    this.Post.getPosts().pipe(
+      mergeMap(() => this.coment.getComent())
+    ).subscribe((res) => {
+      console.log(res);
+      console.log('ok');
+      this.coments = res
+      console.log(this.coments);
+      
+    })
+    
+
   }
 
 }
+
+
+
+
+
+
